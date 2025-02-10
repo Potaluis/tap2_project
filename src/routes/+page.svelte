71c7b5a340966d60f9895637c490f0d92ef3a332
Juan Luis <juanlupriv24@gmail.com>
2025-02-10 16:44:18 +0100

    TaildWind Implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script lang="ts">
+    //TODO
+</script>
+
+<h1>LOG IN</h1>
+<form>
+<input type="text" id="username" name="username" placeholder="Username"><br>
+<input type="password" id="pswd" name="pswd" placeholder="Password"><br>
+<input type="submit" value="Submit">
+</form>
+
+<label>Forgot password?</label>
+
+<!--Tailwindcss will be done after, normal css for placeholder-->
+<!---<style>
+    @reference "tailwindcss/theme";
+    :global(html) {
+      background-color: theme(--color-gray-100);
+    }
+</style>
+-->
