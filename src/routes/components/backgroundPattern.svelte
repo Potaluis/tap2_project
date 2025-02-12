6f989bc75e60c4219a29c99cf169458aba1c6bfb
Juan Luis <juanlupriv24@gmail.com>
2025-02-12 17:47:39 +0100

    backgroundMenuPlaceHolder

---
+++ src/routes/components/backgroundPattern.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<div class="background-base-color"></div>
+
+<style>
+    .background-base-color{
+        position: absolute;
+        background-color: aquamarine;
+        min-height: 100%;
+        width: 100%;
+    }
+</style>
--- src/routes/homepage/+page.svelte
+++ src/routes/homepage/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+    import BackgroundPattern from "../components/backgroundPattern.svelte";
     import Drawer from "../components/drawer.svelte";
     import ProfileDrawer from "../components/profileDrawer.svelte";
 
 </script>
 
-
 <Drawer drawerButtonKind="Drawer" drawerPosition="left"></Drawer>
 
 <ProfileDrawer drawerButtonKind="Profile" drawerPosition="right"></ProfileDrawer>
+
+<BackgroundPattern></BackgroundPattern>
 
 
 
