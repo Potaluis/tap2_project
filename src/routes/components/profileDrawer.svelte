a29f592d1056d535992b71d300a51023fd2b8339
Juan Luis <juanlupriv24@gmail.com>
2025-02-12 19:19:22 +0100

    Drawer navigation implementation

--- src/routes/components/profileDrawer.svelte
+++ src/routes/components/profileDrawer.svelte
@@ -1,6 +1,8 @@
 <!--Drawer example from Svelte Documentation: https://svelte.dev/playground/717ff20010c74d56ad613e17194c8d59?version=5.19.10#H4sIAAAAAAAACq1TwW7bMAz9FYEYkASIrTbZybONDuhuA_oB0w6KxM5qZckwmXRFkH8fJNtIC_SwATsYkB_fIx8p6gxB9wgV3I_6BUeBJwxMsIVH55Gg-nEGfh0SIQGwXehfh6GkE3pO2EETfoSbGDinq6AmM7qBWxUUp88jCzuVbMQnYs243nxZoiYGYhEHDPcLZ71p2rMKmcGTstTWfkt-vztiDDiuV-SLzllcbcU6d7IRTStStuix9PHXepXDm7nUkoi6-DKVD4ovs41aXj2H-nBkjkHEYLwzz835au7SPgwYajkxJjb5Yu7O6wP6Rs3zVSCM10SNgjneYzgqEAcXbMWdo-Zs56yTm5SK0KOZZ5eBJCocYy9O2h-xURAHdjEUtwrah3wUt7V8y_wb9e6q3v27en9V7z9QZ-htJyEjU7fz1PLiVB1q2y5XXXsXnsWYZ0j86pE6RFYguhEfGwUd80CVlMaG8oksencay4Asw9DLO-oiem2wyFK5_N7typvypjgg63L_WVpHLLnDHkmmXS4NkYI2OcobINITaBT00R49KhA0mv9ZeomVT6ns-72Tb4cCW2D8zVDxeMTLz8sfcUA9xL4DAAA=-->
 
 <script lang="ts">
+	import { goto } from '$app/navigation';
+
 	
 	let { drawerButtonKind, drawerPosition }= $props();
 
@@ -16,6 +18,11 @@
 		drawer?.show();
 
 	};
+
+	const navigateTo = (path: string) => {
+        drawer?.hide(); 
+        goto(path);      
+    };
 	
 </script>
 <div class="button-container">
@@ -31,7 +38,7 @@
 		<sl-menu-item value="option-2">Manage Account</sl-menu-item>
 		<sl-menu-item value="option-3">Theme</sl-menu-item>
 		<sl-menu-item value="option-4">Help</sl-menu-item>
-		<sl-menu-item value="option-4">Logout</sl-menu-item>
+		<sl-menu-item value="option-4" onclick={() => navigateTo('../')}>Logout</sl-menu-item>
 	</sl-menu>
 	
 </sl-drawer>
