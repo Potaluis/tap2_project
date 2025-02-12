cc5bcda3d0cd879851aad2e4657480a147e48235
Juan Luis <juanlupriv24@gmail.com>
2025-02-12 15:59:42 +0100

    CenterLogin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,21 @@
 
 <script lang="ts">
-    //TODO
+    import { goto } from '$app/navigation';
+
+    function handleSubmit(event: SubmitEvent) {
+        event.preventDefault();
+        goto('/homepage');
+    }
 </script>
 
+<div class="container">
 <h1>LOG IN</h1>
-<form>
+<form on:submit={handleSubmit}>
 <input type="text" id="username" name="username" placeholder="Username"><br>
 <input type="password" id="pswd" name="pswd" placeholder="Password"><br>
 <input type="submit" value="Submit">
 </form>
-
+</div>
 
 
 <!--Tailwindcss will be done after, normal css for placeholder-->
@@ -22,8 +28,10 @@
 -->
 
 <style>
-    *{
+    .container{
         text-align: center;
+        max-width: 100%;
+        margin-top: 15%;
     }
     h1{
         font-size: 100px;
@@ -46,8 +54,5 @@
         color: black;
         cursor: pointer;
     } 
-    label{
-        
-    }
 
     </style>
