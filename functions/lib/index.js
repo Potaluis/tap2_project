41cd93c5d3b6fa1ab6b9e2c7d8b34df080975099
Juan Luis <juanlupriv24@gmail.com>
2025-02-20 02:07:22 +0100

    FireBaseNewImplementation

---
+++ functions/lib/index.js
@@ -0,0 +1,12 @@
+"use strict";
+// Import function triggers from their respective submodules:
+/**
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+//# sourceMappingURL=index.js.map
